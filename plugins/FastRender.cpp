4b0403d1c2c24ddb6ee8301db665f853f87c14d9
Eliot Janvier <eliot.janvier@epitech.eu>
2023-04-15 10:21:31 +0200

    update: wrote a short plugin for rapid rendering

---
+++ plugins/FastRender.cpp
@@ -0,0 +1,40 @@
+/*
+** EPITECH PROJECT, 2022
+** ray
+** File description:
+** FastRender.cpp
+*/
+
+#include "SFML/Graphics.hpp"
+#include "Ray.hpp"
+#include "PluginManager.hpp"
+
+extern "C" {
+    Plugin *entryPoint()
+    {
+        return new Plugin(
+            nullptr,
+            [](render::Ray &ray, const render::Renderer &rdr) {
+                ray = ray.findIntersections(rdr);
+
+                if (ray.getIntersections().size() == 0) {
+                    ray.setColor(sf::Color {33, 33, 33});
+                    return;
+                }
+                sf::Vector3f intersectionPoint = ray.getIntersections()[0]
+                    .getInterceptPoint();
+                float colorIntensityFromNormal =
+                    (90.0 - ray.getDirection() * ray.getIntersections()[0]
+                        .getInterceptee().getNormal(intersectionPoint)) / 90.0;
+                sf::Color color = sf::Color {
+                    200, 200, 200
+                }
+                ray.setColor(sf::Color(
+                    color * colorIntensityFromNormal,
+                    color * colorIntensityFromNormal,
+                    color * colorIntensityFromNormal));
+            },
+            nullptr,
+        );
+    }
+}
