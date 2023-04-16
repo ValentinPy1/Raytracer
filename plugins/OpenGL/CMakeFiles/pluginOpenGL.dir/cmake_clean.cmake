5257d32d344435fe24a41cb4f263a7390885506d
Eliot Janvier <eliot.janvier@epitech.eu>
2023-04-16 19:28:34 +0200

    update: wrote the code necessary to create, register and use vertex and fragment shaders.

---
+++ plugins/OpenGL/CMakeFiles/pluginOpenGL.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pluginOpenGL.dir/PluginOpenGL.cpp.o"
+  "CMakeFiles/pluginOpenGL.dir/PluginOpenGL.cpp.o.d"
+  "CMakeFiles/pluginOpenGL.dir/glad/src/gl.c.o"
+  "CMakeFiles/pluginOpenGL.dir/glad/src/gl.c.o.d"
+  "CMakeFiles/pluginOpenGL.dir/home/Amour/Documents/Epitech/Tech2/ray/src/DLLoader.cpp.o"
+  "CMakeFiles/pluginOpenGL.dir/home/Amour/Documents/Epitech/Tech2/ray/src/DLLoader.cpp.o.d"
+  "CMakeFiles/pluginOpenGL.dir/home/Amour/Documents/Epitech/Tech2/ray/src/Plugin.cpp.o"
+  "CMakeFiles/pluginOpenGL.dir/home/Amour/Documents/Epitech/Tech2/ray/src/Plugin.cpp.o.d"
+  "CMakeFiles/pluginOpenGL.dir/home/Amour/Documents/Epitech/Tech2/ray/src/PluginManager.cpp.o"
+  "CMakeFiles/pluginOpenGL.dir/home/Amour/Documents/Epitech/Tech2/ray/src/PluginManager.cpp.o.d"
+  "CMakeFiles/pluginOpenGL.dir/opgl_main_test.cpp.o"
+  "CMakeFiles/pluginOpenGL.dir/opgl_main_test.cpp.o.d"
+  "pluginOpenGL"
+  "pluginOpenGL.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/pluginOpenGL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
