5e5706d886b33235fd3bd6f845e5149bbf36cd5e
Eliot Janvier <eliot.janvier@epitech.eu>
2023-04-18 10:13:45 +0200

    add: an exemple config file for reference

--- cmake_install.cmake
+++ cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /workdir/Epitech/2Tek/oop/raytracer
+# Install script for directory: /home/Amour/Documents/Epitech/Tech2/ray
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/workdir/Epitech/2Tek/oop/raytracer/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/Amour/Documents/Epitech/Tech2/ray/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
